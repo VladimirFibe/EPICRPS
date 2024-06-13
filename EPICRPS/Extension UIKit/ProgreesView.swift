8146ce1f8c1f8b3a27bcb17c7053c695de96e83a
SemenShevchik <s_he_f@bk.ru>
2024-06-13 11:48:21 +0300

    Верстка экрана

---
+++ EPICRPS/Extension UIKit/ProgreesView.swift
@@ -0,0 +1,28 @@
+//
+//  ProgreesView.swift
+//  EPICRPS
+//
+//  Created by Семен Шевчик on 12.06.2024.
+//
+//
+//import UIKit
+//
+//import UIKit
+//
+//class VerticalProgressView: UIProgressView {
+//    override init(frame: CGRect) {
+//        super.init(frame: frame)
+//        self.transform = CGAffineTransform(rotationAngle: .pi / 2)
+//    }
+//    
+//    required init?(coder: NSCoder) {
+//        super.init(coder: coder)
+//        self.transform = CGAffineTransform(rotationAngle: .pi / 2)
+//    }
+//    
+//    override func layoutSubviews() {
+//        super.layoutSubviews()
+//    }
+//}
+
+
